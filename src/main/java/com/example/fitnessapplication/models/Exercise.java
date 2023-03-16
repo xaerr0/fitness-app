94666eebdee027402f8bb4d4ecc74520dcce431a
Danny Brown <xaerr00@gmail.com>
2023-03-16 00:15:38 -0700

    first commit

---
+++ src/main/java/com/example/fitnessapplication/models/Exercise.java
@@ -0,0 +1,12 @@
+package com.example.fitnessapplication.models;
+
+public class Exercise {
+
+    private long id;
+    private String bodyPart;
+    private String equipment;
+    private String exerciseGif;
+    private String name;
+    private String target;
+
+}
