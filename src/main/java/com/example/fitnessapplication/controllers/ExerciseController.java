b2b6c4173163b774e102d8944450ba19239974d4
Danny Brown <xaerr00@gmail.com>
2023-03-17 17:26:41 -0700

    Grab All Exercises

--- src/main/java/com/example/fitnessapplication/controllers/ExerciseController.java
+++ src/main/java/com/example/fitnessapplication/controllers/ExerciseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -22,4 +23,17 @@ public class ExerciseController {
     public Exercise[] getAllExercises() {
         return exerciseService.getAllExercises();
     }
+    @ResponseBody
+    @GetMapping("/exercises/{id}")
+    public Exercise getExercise(@PathVariable Long id) {
+        return exerciseService.getExercise(id);
+    }
+
+    @ResponseBody
+    @GetMapping("/exercises/name/{name}")
+    public Exercise[] getExercisesByName(@PathVariable String name) {
+        return exerciseService.getExerciseByName(name);
+    }
+
+
 }
--- src/main/java/com/example/fitnessapplication/services/ExerciseService.java
+++ src/main/java/com/example/fitnessapplication/services/ExerciseService.java
@@ -1,7 +1,6 @@
 package com.example.fitnessapplication.services;
 
 import com.example.fitnessapplication.models.Exercise;
-import com.example.fitnessapplication.models.ExerciseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
@@ -10,8 +9,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class ExerciseService {
@@ -42,11 +39,51 @@ public class ExerciseService {
         ResponseEntity<Exercise[]> response =
                 restTemplate.exchange(uri, HttpMethod.GET, httpEntity, Exercise[].class);
 
-
-        if (response.getStatusCode().equals(HttpStatus.OK)) {
+        if (response.getStatusCode().equals(HttpStatus.OK) && response.getBody() != null) {
             return response.getBody();
         } else {
             return null;
         }
     }
-}
+
+    //TODO Fix this
+    public Exercise getExercise(Long id) {
+//        String paddedId = String.format("%04d", id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-RapidAPI-Key", apiKey);
+        headers.set("X-RapidAPI-Host", apiHost);
+
+        HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+        URI uri = UriComponentsBuilder.fromUriString("https://exercisedb.p.rapidapi.com/exercises/exercise/" + id)
+                .build()
+                .toUri();
+
+        ResponseEntity<Exercise> response = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, Exercise.class);
+        if (response.getStatusCode().equals(HttpStatus.OK) && response.getBody() != null) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }
+
+
+    public Exercise[] getExerciseByName(String name) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-RapidAPI-Key", apiKey);
+        headers.set("X-RapidAPI-Host", apiHost);
+
+        HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+        URI uri = UriComponentsBuilder.fromUriString("https://exercisedb.p.rapidapi.com/exercises/name/" + name)
+                .build()
+                .toUri();
+
+        ResponseEntity<Exercise[]> response =
+                restTemplate.exchange(uri, HttpMethod.GET, httpEntity, Exercise[].class);
+        if (response.getStatusCode().equals(HttpStatus.OK) && response.getBody() != null) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }
+    }
