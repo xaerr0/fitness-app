041c71936d7f71fa6feffa591f8c6c4795998b77
Danny Brown <xaerr00@gmail.com>
2023-03-19 00:30:50 -0700

    Equipment Service/Controller

---
+++ src/main/java/com/example/fitnessapplication/controllers/EquipmentController.java
@@ -0,0 +1,25 @@
+package com.example.fitnessapplication.controllers;
+
+import com.example.fitnessapplication.models.Equipment;
+import com.example.fitnessapplication.services.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/equipment")
+public class EquipmentController {
+
+    @Autowired
+    EquipmentService equipmentService;
+
+    @ResponseBody
+    @GetMapping
+    public List<Equipment> getAllEquipment() {
+        return equipmentService.getAllEquipment();
+    }
+}
--- src/main/java/com/example/fitnessapplication/models/Equipment.java
+++ src/main/java/com/example/fitnessapplication/models/Equipment.java
@@ -1,8 +1,16 @@
 package com.example.fitnessapplication.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Equipment {
 
     private Long id;
-
-    private String name;
+    private String equipment;
 }
