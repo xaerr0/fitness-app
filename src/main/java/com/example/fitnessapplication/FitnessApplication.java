e1699501abf20ec3d5c4d59f7fb3187bf5bd1ba9
Danny Brown <xaerr00@gmail.com>
2023-03-29 22:43:54 -0700

    Update

--- src/main/java/com/example/fitnessapplication/FitnessApplication.java
+++ src/main/java/com/example/fitnessapplication/FitnessApplication.java
@@ -1,13 +1,23 @@
 package com.example.fitnessapplication;
 
+import com.example.fitnessapplication.models.Exercise;
+import com.example.fitnessapplication.repos.ExerciseRepo;
+import com.example.fitnessapplication.services.ExerciseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @SpringBootApplication
 public class FitnessApplication {
+
+	@Autowired
+	ExerciseRepo exerciseRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessApplication.class, args);
@@ -20,4 +30,12 @@ public class FitnessApplication {
 		return builder.build();
 	}
 
+	@Bean
+	public CommandLineRunner run() throws Exception {
+		return args -> {
+			List<Exercise> exercises = exerciseRepo.findAll();
+			exerciseRepo.saveAll(exercises);
+			exercises.forEach(System.out::println);
+        };
+	}
 }
