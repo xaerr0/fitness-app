6d10001263da72cc5247310885fd5f9039c1869d
Danny Brown <xaerr00@gmail.com>
2023-04-04 00:36:30 -0700

    Login and Workout Generator page

--- src/main/java/com/example/fitnessapplication/controllers/UserController.java
+++ src/main/java/com/example/fitnessapplication/controllers/UserController.java
@@ -1,16 +1,45 @@
 package com.example.fitnessapplication.controllers;
 
+import com.example.fitnessapplication.models.BodyPart;
+import com.example.fitnessapplication.models.Equipment;
+import com.example.fitnessapplication.models.Target;
+import com.example.fitnessapplication.services.BodyPartService;
+import com.example.fitnessapplication.services.EquipmentService;
+import com.example.fitnessapplication.services.TargetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping("/login")
+    @Autowired
+    EquipmentService equipmentService;
+
+    @Autowired
+    BodyPartService bodypartService;
+
+    @Autowired
+    TargetService targetService;
+
+    @GetMapping("/register")
     public String login() {
-        return "/login";
+        return "/register";
     }
 
+    @GetMapping("/generator")
+    public String generatorPage(Model model) {
+        List<Equipment> equipmentList = equipmentService.getAllEquipment();
+        model.addAttribute("equipmentList", equipmentList);
+        List<BodyPart> bodyPartList = bodypartService.getAllBodyParts();
+        model.addAttribute("bodyPartList", bodyPartList);
+        List<Target> targetList = targetService.getAllTargetMuscles();
+        model.addAttribute("targetList", targetList);
+        return "generator";
+    }
 }
