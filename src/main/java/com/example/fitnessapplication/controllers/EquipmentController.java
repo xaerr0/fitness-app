e58aa62547ab29a350f071a852151bcf69973c50
Danny Brown <xaerr00@gmail.com>
2023-04-04 00:37:09 -0700

    cleaned

--- src/main/java/com/example/fitnessapplication/controllers/EquipmentController.java
+++ src/main/java/com/example/fitnessapplication/controllers/EquipmentController.java
@@ -4,7 +4,9 @@ import com.example.fitnessapplication.models.Equipment;
 import com.example.fitnessapplication.services.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
