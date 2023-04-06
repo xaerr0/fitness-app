0e770db8d6b23b87713badccf68cad880ce7deb0
Danny Brown <xaerr00@gmail.com>
2023-04-05 22:12:55 -0700

    Method to capitalize every word from json

---
+++ src/main/java/com/example/fitnessapplication/services/CapitalizeEveryWord.java
@@ -0,0 +1,12 @@
+package com.example.fitnessapplication.services;
+
+public class CapitalizeEveryWord {
+
+    public String capitalizeEveryWord(String str) {
+        String[] words = str.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
+        }
+        return String.join(" ", words);
+    }
+}
