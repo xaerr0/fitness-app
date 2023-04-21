bd3c70c0212a250b7ca5f2ba8918ea383f720d5f
Danny Brown <xaerr00@gmail.com>
2023-04-20 23:55:13 -0700

    User Class

---
+++ src/main/java/com/example/fitnessapplication/controllers/User.java
@@ -0,0 +1,27 @@
+package com.example.fitnessapplication.controllers;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
