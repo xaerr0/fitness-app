f0e88f0f2694fb5e9cbd141c6f44167dbb2f8d7c
Danny Brown <xaerr00@gmail.com>
2023-04-26 23:24:45 -0700

    Cleaning code

--- src/main/java/com/example/fitnessapplication/controllers/ExerciseController.java
+++ src/main/java/com/example/fitnessapplication/controllers/ExerciseController.java
@@ -5,7 +5,10 @@ import com.example.fitnessapplication.services.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- src/main/java/com/example/fitnessapplication/services/BodyGroupService.java
+++ src/main/java/com/example/fitnessapplication/services/BodyGroupService.java
@@ -1,23 +1,15 @@
 package com.example.fitnessapplication.services;
 
 
-import com.example.fitnessapplication.models.Exercise;
 import com.example.fitnessapplication.repos.ExerciseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
 @Service
-
 public class BodyGroupService {
 
     @Autowired
@@ -43,5 +35,4 @@ public class BodyGroupService {
         return Map.of("Upper Body", List.of("chest", "back", "shoulders", "neck", "upper arms", "lower arms"),
                 "Lower Body", List.of("lower legs", "upper legs", "waist"));
     }
-
 }
