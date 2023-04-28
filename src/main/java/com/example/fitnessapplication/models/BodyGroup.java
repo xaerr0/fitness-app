ecbd4a4294a1300aa9201a6efd50f1ce15ea9937
Danny Brown <xaerr00@gmail.com>
2023-04-27 19:33:12 -0700

    Added Map for bodygroup

---
+++ src/main/java/com/example/fitnessapplication/models/BodyGroup.java
@@ -0,0 +1,11 @@
+package com.example.fitnessapplication.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BodyGroup {
+    private String bodyGroup;
+}
--- src/main/java/com/example/fitnessapplication/services/BodyGroupService.java
+++ src/main/java/com/example/fitnessapplication/services/BodyGroupService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -32,7 +33,13 @@ public class BodyGroupService {
     //TODO Build out the rest of the map
     public Map<String, List<String>> getGroupMap() {
 
-        return Map.of("Upper Body", List.of("chest", "back", "shoulders", "neck", "upper arms", "lower arms"),
-                "Lower Body", List.of("lower legs", "upper legs", "waist"));
+        return Map.of(
+                "Upper Body", List.of("chest", "back", "shoulders", "neck", "upper arms", "lower arms"),
+                "Lower Body", List.of("lower legs", "upper legs"),
+                "Full Body", List.of("chest", "back", "shoulders", "neck", "upper arms", "lower arms",
+                        "lower legs", "upper legs"),
+                "Cardio", Collections.singletonList("cardio"),
+
+                "Abs", Collections.singletonList("waist"));
     }
 }
