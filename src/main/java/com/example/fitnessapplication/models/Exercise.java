9b5d553dc575846fe032cb3b97c0bc82bf37d0d5
Danny Brown <xaerr00@gmail.com>
2023-05-05 23:26:47 -0700

    refactor

--- src/main/java/com/example/fitnessapplication/models/Exercise.java
+++ src/main/java/com/example/fitnessapplication/models/Exercise.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
