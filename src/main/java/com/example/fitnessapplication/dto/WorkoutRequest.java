016813a3223e0a0552b96e755261631538f17b50
Danny Brown <xaerr00@gmail.com>
2023-05-05 23:29:14 -0700

    fixed

--- src/main/java/com/example/fitnessapplication/dto/WorkoutRequest.java
+++ src/main/java/com/example/fitnessapplication/dto/WorkoutRequest.java
@@ -2,6 +2,7 @@ package com.example.fitnessapplication.dto;
 
 
 import com.example.fitnessapplication.models.BodyGroup;
+import com.example.fitnessapplication.models.Equipment;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,7 @@ import java.util.List;
 
 public class WorkoutRequest {
 
-    List<String> equipment;
-    BodyGroup bodyGroup;
+    private List<Equipment> equipment;
+    private BodyGroup bodyGroup;
 
 }
