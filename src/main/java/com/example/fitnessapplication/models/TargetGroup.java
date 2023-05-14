c203beb7b67d81aaa3ba324d4d62dc884b70f992
Danny Brown <xaerr00@gmail.com>
2023-05-14 00:51:31 -0700

    created target groups class

---
+++ src/main/java/com/example/fitnessapplication/models/TargetGroup.java
@@ -0,0 +1,12 @@
+package com.example.fitnessapplication.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TargetGroup {
+    private String name;
+}
