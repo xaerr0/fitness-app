059ab9a5fa60604d0e13d45c5a211bd76764e906
Danny Brown <xaerr00@gmail.com>
2023-05-14 00:51:11 -0700

    added target groups

--- src/main/java/com/example/fitnessapplication/models/Target.java
+++ src/main/java/com/example/fitnessapplication/models/Target.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
 
 //@Entity
 @Getter
@@ -18,11 +22,34 @@ public class Target {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-//    @OneToOne
     private String name;
+
+    private List<TargetGroup> groups;
 
     public Target(String target) {
         this.name = target;
+        groups = new ArrayList<>();
+        TargetGroup upperBody = new TargetGroup("Upper Body Muscles");
+        TargetGroup lowerBody = new TargetGroup("Lower Body Muscles");
+        TargetGroup fullBody = new TargetGroup("Full Body Muscles");
+
+        if (name.equals("biceps") || name.equals("delts") || name.equals("forearms") || name.equals("lats") ||
+            name.equals("levator scapulae") || name.equals("pectorals") || name.equals("serratus anterior") ||
+            name.equals("spine") || name.equals("traps") || name.equals("triceps") || name.equals("upper back")) {
+            groups.add(upperBody);
+            groups.add(fullBody);
+        }
+        if (name.equals("abductors") || name.equals("adductors") || name.equals("calves") || name.equals("glutes") ||
+            name.equals("hamstrings") || name.equals("quads")) {
+            groups.add(lowerBody);
+            groups.add(fullBody);
+        }
+        if (name.equals("cardiovascular system")) {
+            groups.add(new TargetGroup("Cardio"));
+        }
+        if (name.equals("abs")) {
+            groups.add(new TargetGroup("Abs"));
+        }
     }
 
     @Override
@@ -30,7 +57,7 @@ public class Target {
         // API is lowercase - this capitalizes the first letter of every word
         String[] words = name.split(" ");
         for (int i = 0; i < words.length; i++) {
-            words[i] = words[i].substring(0,1).toUpperCase() + words[i].substring(1);
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
         }
         return String.join(" ", words);
     }
