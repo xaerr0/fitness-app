7449735b186205a7450559408f3b33366b487877
Danny Brown <xaerr00@gmail.com>
2023-05-18 00:10:31 -0700

    added login and register page

--- src/main/java/com/example/fitnessapplication/controllers/UserController.java
+++ src/main/java/com/example/fitnessapplication/controllers/UserController.java
@@ -2,14 +2,18 @@ package com.example.fitnessapplication.controllers;
 
 import com.example.fitnessapplication.dto.WorkoutRequest;
 import com.example.fitnessapplication.models.*;
+import com.example.fitnessapplication.models.securitymodels.CustomUserDetails;
 import com.example.fitnessapplication.services.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -32,10 +36,30 @@ public class UserController {
     @Autowired
     BodyGroupService bodyGroupService;
 
+    @Autowired
+    CustomUserDetailsService userDetailsService;
+
+
+
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
 
     @GetMapping("/register")
-    public String login() {
+    public String register(Model model) {
+        CustomUserDetails user = new CustomUserDetails();
+        user.setUserMeta(new UserMeta());
+        model.addAttribute("user", user);
+
         return "/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") CustomUserDetails user) {
+        userDetailsService.createNewUser(user);
+        return "redirect:/generator-test";
     }
 
     @GetMapping("/generator")
@@ -67,5 +91,21 @@ public class UserController {
         List<Exercise> exerciseRequest = exerciseService.getExercises(workoutRequest);
         model.addAttribute("exerciseRequest", exerciseRequest);
         return "workout";
+    }
+
+    @GetMapping("/user")
+    public CustomUserDetails getUser(Authentication authentication) {
+        return (CustomUserDetails) authentication.getPrincipal();
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<?> createNewUser(@RequestBody CustomUserDetails userDetails) {
+        try {
+            return ResponseEntity.ok(userDetailsService.createNewUser(userDetails));
+        } catch (IllegalStateException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 }
