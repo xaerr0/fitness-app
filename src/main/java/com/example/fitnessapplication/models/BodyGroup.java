d29e6aa4151bcbcc7664f348a101d4f1b27a8435
Danny Brown <xaerr00@gmail.com>
2023-05-17 17:45:15 -0700

    refactor

--- src/main/java/com/example/fitnessapplication/models/BodyGroup.java
+++ src/main/java/com/example/fitnessapplication/models/BodyGroup.java
@@ -3,11 +3,17 @@ package com.example.fitnessapplication.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class BodyGroup {
     private String name;
+
+    public String getName() {
+        return name;
+    }
 }
