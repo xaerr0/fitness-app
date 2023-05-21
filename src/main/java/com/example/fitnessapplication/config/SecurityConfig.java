7fa5cd53af10568f1bb702c4d6950c6b8800b3ad
Danny Brown <xaerr00@gmail.com>
2023-05-21 01:06:45 -0700

    fixed filterchain

--- src/main/java/com/example/fitnessapplication/config/SecurityConfig.java
+++ src/main/java/com/example/fitnessapplication/config/SecurityConfig.java
@@ -3,35 +3,62 @@ package com.example.fitnessapplication.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+//@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
+
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                //disable CSRF for Postman usage
-                .csrf(csrf -> csrf.disable())
-                //authorize all requests to access CSS and JavaScript
-                .authorizeRequests(auth -> auth
-                        .antMatchers("/static/css", "static/js").permitAll()
-                        //allow all requests to read recipes and reviews
-                        .antMatchers(HttpMethod.GET, "/generator", "/generated", "/register", "/login").permitAll()
-                        //allow creation of new recipes and reviews
-//                        .antMatchers(HttpMethod.POST, "/generator", "/generated").permitAll()
-                        //all other requests should be authenticated
-                        .anyRequest().permitAll());
-                //users should log in with HTTP Basic.
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/static/**", "/webjars/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/register", "/login", "/static/css", "static/js").permitAll()
+                .anyRequest().hasRole("USER")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+
 
         return http.build();
+
     }
+
+//    @Bean
+//    public CsrfSec filterChain(HttpSecurity httpSecurity) throws Exception {
+//        HttpSecurity
+//                //disable CSRF for Postman usage
+//                .csrf().disable()
+//                //authorize all requests to access CSS and JavaScript
+//                .authorizeRequests(auth -> auth
+//                        .antMatchers("/static/css", "static/js").permitAll()
+//                        //allow all requests to read recipes and reviews
+//                        .antMatchers(HttpMethod.GET, "/register", "/login", "/static/css", "static/js").permitAll()
+//                        .antMatchers("/generator").hasRole("USER"))
+//                .formLogin()
+//                .loginPage("/login").permitAll();
+//
+//        //users should log in with HTTP Basic.
+//
+//        return httpSecurity.build();
+//    }
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
