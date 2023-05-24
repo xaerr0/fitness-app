e3bfcbfecb7869bf76dcfb6bb233650f924b0bd5
Danny Brown <xaerr00@gmail.com>
2023-05-24 00:16:10 -0700

    refactor

--- src/main/java/com/example/fitnessapplication/services/BodyPartService.java
+++ src/main/java/com/example/fitnessapplication/services/BodyPartService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.annotation.PostConstruct;
 import java.net.URI;
 import java.util.List;
 
